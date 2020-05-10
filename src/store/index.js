5a5c7f6035e15119c4651925f7633598683ef124
叶洪峰 <1498244872@qq.com>
2020-05-10 15:04:02 +0800

    优化了本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '../utils/storage'
 
 Vue.use(Vuex)
-
+const USER_KEY = 'toutiao-user'
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem('user')) // 当前登录用户的登录状态(token等数据)
+    user: getItem(USER_KEY) // 当前登录用户的登录状态(token等数据)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       // 把数据放到本地存储
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem(USER_KEY, state.user)
     }
   },
   actions: {

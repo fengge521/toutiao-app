819954a65284c0a4c9f5b390b3f1038e645e3162
叶洪峰 <1498244872@qq.com>
2020-05-10 15:55:02 +0800

    个人中心头部布局

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="my-container">
-    我的页面
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" center>
+        <van-image round slot="icon" fit="cover" width="50" height="50" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <div slot="title">昵称</div>
+        <van-button size="small" round>编辑资料</van-button>
+      </van-cell>
+    </van-cell-group>
   </div>
 </template>
 

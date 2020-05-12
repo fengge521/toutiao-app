bcf52f42036d2a3e289a0f7f6bf2fe6efbfabd8c
叶洪峰 <1498244872@qq.com>
2020-05-12 16:29:03 +0800

    更改我的里面的样式以及首页的布局

--- src/api/user.js
+++ src/api/user.js
@@ -4,7 +4,7 @@
 import request from '@/utils/request'
 
 /***
- * 登录
+ * 登录 / 注册
  */
 export const login = data => {
   return request({
@@ -20,5 +20,23 @@ export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+/**
+ * 获取登录用户信息
+ */
+export const getCurrentUser = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
+  })
+}
+/**
+ * 获取用户频道列表
+ */
+export const getUserChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/channels'
   })
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,29 +1,79 @@
 <template>
   <div class="home-container">
-    首页
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        icon="search"
+        type="info"
+        round
+        size="small"
+        class="search-btn"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- 导航栏 -->
+    <!-- 频道列表 -->
+    <van-tabs v-model="active">
+      <van-tab
+        :title="channel.name"
+        v-for="channel in channels"
+        :key="channel.id"
+      >
+        <!-- 文章列表 -->
+        <article-list :channel="channel" />
+        <!-- /文章列表 -->
+      </van-tab>
+    </van-tabs>
+    <!-- 频道列表 -->
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+import ArticleList from './components/article-list'
 export default {
   name: 'HomeIndex',
+  components: {
+    ArticleList
+  },
   data () {
     return {
-
+      active: 0, // 控制被激活的标签
+      channels: [] // 频道列表
     }
   },
   created () {
-
+    this.loadChannels()
   },
   mounted () {
 
   },
   methods: {
-
+    async loadChannels () {
+      // 请求获取频道数据
+      const { data } = await getUserChannels()
+      this.channels = data.data.channels
+    }
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
 </style>

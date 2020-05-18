c055da9a41983ade0297eaee067c89ce7ea6f9a0
叶洪峰 <1498244872@qq.com>
2020-05-18 22:15:00 +0800

    一级评论列表

--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="comment-list">
     <van-cell title="全部评论"></van-cell>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="(comment, index) in list"
+        :key="index"
+        :title="comment.content" />
+    </van-list>
   </div>
 </template>
 
 <script>
+import { getComments } from '@/api/comment'
 export default {
-  name: 'commentList',
+  name: 'CommentList',
+  props: {
+    source: {
+      type: [Number, String, Object],
+      required: true
+    }
+  },
   data () {
     return {
-
+      list: [],
+      loading: false,
+      finished: false,
+      offset: null, // 获取下一页数据的页码
+      limit: 10 // 每页大小
     }
   },
   created () {
@@ -19,7 +41,29 @@ export default {
 
   },
   methods: {
-
+    async onLoad () {
+      // 1. 请求获取数据
+      const { data } = await getComments({
+        type: 'a', // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
+        source: this.source, // 源id，文章id或评论id
+        offset: this.offset, // 获取评论数据的偏移量，值为评论id，表示从此id的数据向后取，不传表示从第一页开始读取数据
+        limit: this.limit // 每页大小
+      })
+      console.log(data)
+      // 2. 把数据放到数据列表中
+      const { results } = data.data
+      this.list.push(...results)
+      // 3.将本次loading关闭
+      this.loading = false
+      // 4. 判断是否还有数据
+      if (results.length) {
+        // 如果有，更新获取下一页数据的页码
+        this.offset = data.data.last_id
+      } else {
+        // 如果没有，则将 finished 设置为 ture，不再触发加载更多了
+        this.finished = true
+      }
+    }
   }
 }
 </script>
